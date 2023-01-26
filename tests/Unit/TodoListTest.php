b7c23a5cc94c5039e5f37fc8d496531db7ca88c8
Tancrède SUARD <gitlab.commits@tancou.me>
2023-01-26 22:46:04 +0100

    Test TodoList having multiple tasks

--- tests/Unit/TodoListTest.php
+++ tests/Unit/TodoListTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Models\Task;
 use App\Models\TodoList;
 use Illuminate\Testing\Assert;
 use Tests\TestCase;
@@ -33,5 +34,22 @@ class TodoListTest extends TestCase
         Assert::assertEquals('la description', $todoList->description);
         Assert::assertEquals($due_date, $todoList->due_date);
         Assert::assertEquals(1, $todoList->priority);
+    }
+
+    public function test_can_have_no_task()
+    {
+        $todoList = TodoList::factory()->create();
+        Assert::assertEquals(0, $todoList->tasks()->count());
+    }
+
+    public function test_has_tasks()
+    {
+        $todoList = TodoList::factory()
+            ->has(
+                Task::factory()->count(2)
+            )
+            ->create();
+        Assert::assertGreaterThan(0, $todoList->tasks()->count());
+        Assert::assertEquals(2, $todoList->tasks->count());
     }
 }
