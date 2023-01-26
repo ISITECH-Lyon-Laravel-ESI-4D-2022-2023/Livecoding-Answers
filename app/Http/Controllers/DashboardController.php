33f30fd90109ea471e295046f564f824dfe8dd6b
Tancrède SUARD <gitlab.commits@tancou.me>
2023-01-26 21:04:26 +0100

    Return json of todolists in dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TodoList;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -10,10 +11,11 @@ class DashboardController extends Controller
      * Handle the incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function __invoke(Request $request)
     {
-        //
+        $todoLists = TodoList::query()->get();
+        return response()->json($todoLists);
     }
 }
